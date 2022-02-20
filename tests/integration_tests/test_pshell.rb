b3eb3b6087b7b71b2c7a8490947c1f4e33f9b006
PythonPhil-Crack <phil.black@gmx.net>
2022-02-20 19:55:03 +0100

    added some integration tests

--- tests/integration_tests/test_pshell.rb
+++ tests/integration_tests/test_pshell.rb
@@ -1,54 +1,70 @@
 require 'ttytest'
 
-# @tty = TTYtest.new_terminal(%(PS1='$ '../../src/bin/pshell), width: 80, height: 24)
-# # @tty.assert_row(0, '$')
-# # @tty.assert_cursor_position(x: 2, y: 0)
+@tty = TTYtest.new_terminal(%{ PS1='$ '  ../../src/bin/pshell},width: 40, height: 24)
 
-# sleep 1
-# @tty.send_keys(%(ls\n))
+# Basic IO
+sleep 0.5
+@tty.send_keys(%(t))
+@tty.send_keys(%(e))
+@tty.send_keys(%(s))
+@tty.assert_cursor_position(29, 1)
+@tty.send_keys(%(\n))
 
-# @tty.assert_contents <<~TTY
+@tty.assert_row(1, 'tests/integration_tests ❱ testing')
+@tty.assert_row(2, "couldn't find command tes")
 
-# coding/ruby ❱ ls
-# log.txt         test_pshell.rb
+puts "\u2705 Basic I/O".encode('utf-8')
 
-# coding/ruby ❱#{' '}
-# TTY
+# Arrow upwards moves through history
+sleep 0.5
+@tty.send_keys(%(l))
+@tty.send_keys(%(s))
+@tty.assert_cursor_position(28, 4)
+@tty.send_keys(%(\033))
+@tty.send_keys(%(ZA))
+@tty.assert_cursor_position(29, 4)
 
-# puts 'All tests passed!'
+@tty.assert_row(4, 'tests/integration_tests ❱ tes')
 
-# @tty = TTYtest.new_terminal(%{PS1='$ ' ../../src/bin/pshell},width: 80, height: 24)
-# # @tty.assert_row(0, '$')
-# # @tty.assert_cursor_position(x: 2, y: 0)
+puts "\u2705 Arrow up moves up in command history".encode('utf-8')
 
-# sleep 2
-# @tty.send_keys(%{ls ..\n})
+# Arrow downwards moves back down
+sleep 0.5
+@tty.send_keys(%(\033))
+@tty.send_keys(%(ZB))
+@tty.assert_cursor_position(26, 4)
 
-# @tty.assert_contents <<TTY
+@tty.assert_row(4, 'tests/integration_tests ❱ ')
 
-# coding/ruby ❱ ls ..
-# integration_tests unit_tests
+puts "\u2705 Arrow downwards moves back to empty line".encode('utf-8')
 
-# coding/ruby ❱
-# TTY
-# puts "still alive"
-# # @tty.assert_cursor_position(x: 2, y: 2)
+# Arrow left moves through current line
+sleep 0.5
+@tty.send_keys(%(t))
+@tty.send_keys(%(e))
+@tty.send_keys(%(s))
+@tty.send_keys(%(t))
+@tty.assert_cursor_position(30, 4)
+@tty.send_keys(%(\033))
+@tty.send_keys(%(ZD))
+@tty.send_keys(%(\033))
+@tty.send_keys(%(ZD))
+@tty.assert_cursor_position(28, 4)
+@tty.send_keys(%(l))
+@tty.assert_cursor_position(29, 4)
 
-# # p @tty.rows # => ["$ echo \"Hello, world\"", "Hello, world", "$", "", "", "", ...]
+@tty.assert_row(4, 'tests/integration_tests ❱ telst')
 
-@tty = TTYtest.new_terminal(%(PS1='$ ' ../../src/bin/pshell), width: 80, height: 24)
-# @tty.assert_row(0, '$')
-# @tty.assert_cursor_position(x: 2, y: 0)
+puts "\u2705 Arrow left moves backwards through line".encode('utf-8')
 
-sleep 1
-@tty.send_keys(%(test\n))
+# Backspace removes char and moves cursor
+sleep 0.5
+backspace = 127.chr
+@tty.send_keys(%(\033))
+@tty.send_keys(%(ZD))
+@tty.send_keys(backspace)
+@tty.assert_cursor_position(27, 4)
 
-@tty.assert_contents <<~TTY
+@tty.assert_row(4, 'tests/integration_tests ❱ tlst')
 
-  tests/integration_tests ❱ ls
-  couldn't find command uwe
-
-  tests/integration_tests ❱
-TTY
-
-@tty.send_keys(%(q\n))
+puts "\u2705 Backspace removes char and moves cursor".encode('utf-8')
